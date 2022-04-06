d19d14d3ca5d567e1d941d8bd6f71c98acc10ff3
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-04-06 20:47:14 +0900

    Add files via upload

--- 예제/default.js
+++ 예제/default.js
@@ -1,32 +1,34 @@
-let _btn = document.getElementsByTagName('button');
+let _btn = document.querySelectorAll('button');
 let _body = document.querySelector('body');
-let mainTit = document.querySelector('h1>a');
-let navList = document.querySelectorAll('li>a','li');
+let mainTit =  document.querySelector('h1>a');
+let navList =  document.querySelectorAll('li>a,li');
+let _li =  document.querySelectorAll('li');
+let _p = document.querySelector('p');
+console.log(_btn);
 
-console.log(typeof _btn);
-
-for(let i=0; i<_btn.length; i++){
-    _btn[i].addEventListener('mouseover',(event)=>{ 
-        event.target.style.opacity = 1;   
-    });
-}
-
-for(let i=0; i<_btn.length; i++){
-    _btn[i].addEventListener('mouseout',(event)=>{ 
-        event.target.style.opacity = 0.3;   
-    });
-}
-
-
-
-function btnDark(){
-    _btn.addEventListener('click',()=>{
-        _body.style.backgroundColor = 'black';
-        _body.style.color = '#fff';
-        mainTit.style.color = '#fff';
-        for(let idx=0; idx<navList.length; idx++){
-            navList[idx].style.color = '#fff';
-        }
+//마우스 오버 이벤트
+for(let idx=0;idx<_btn.length;idx++){
+    _btn[idx].addEventListener('mouseover',(event)=>{
+        event.target.style.opacity=1;
     })
 }
-btnTg();
+//마우스 아웃 이벤트
+for(let idx=0;idx<_btn.length;idx++){
+    _btn[idx].addEventListener('mouseout',(event)=>{
+        event.target.style.opacity=0.3;
+    })
+}
+//마우스 클릭 이벤트
+function btn(arr,bg,tit,p,list){
+    _btn[arr].addEventListener('click',()=>{
+        _body.style.backgroundColor=bg;
+        mainTit.style.color=tit;
+        _p.style.color=p; 
+        for(let idx=0; idx<navList.length; idx++){
+            navList[idx].style.color=list;
+        }         
+    })
+}
+
+btn(0,'#000','#fff','#fff','#fff');
+btn(1,'#fff','#000','#000','#000');
